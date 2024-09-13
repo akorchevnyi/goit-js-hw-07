0d32c5267fc9469b8e15d230314edeef8709702b
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2024-09-13 12:19:17 +0300

    process error

--- js/task-5.js
+++ js/task-5.js
@@ -1,19 +1,22 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
         .toString(16)
-        .padStart(6, 0)}`;
+        .padStart(6, "0")}`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const bodyElement = document.querySelector("body");
     const btn = document.querySelector(".change-color");
+    const spanElement = document.querySelector(".color");
 
     if (!btn || !bodyElement) {
         console.warn("Failed to find the elements");
     }
     else {
         btn.addEventListener("click", () => {
-            bodyElement.style.backgroundColor = getRandomHexColor();
+            const newColor = getRandomHexColor();
+            bodyElement.style.backgroundColor = newColor;
+            spanElement.textContent = newColor;
         });
     }
 });
